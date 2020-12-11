21b33200e100b699a334f60c012abafe1d84fc77
naomischofield <naomi_s257@hotmail.com>
2020-12-11 15:26:01 +0000

    Added two pair, three of a kind, four of a kind

--- PokerKata/PlayerHand.cs
+++ PokerKata/PlayerHand.cs
@@ -7,33 +7,92 @@ namespace PokerKata
     internal class PlayerHand
     {
         private List<PlayerCard> PlayingCards = new List<PlayerCard>();
-        
+        IOrderedEnumerable<IGrouping<Rank, PlayerCard>> groupedByRank;
+
+
         public PlayerHand(string playersHand)
         {
             var arrayOfCards = playersHand.Split(" ");
             foreach (var card in arrayOfCards)
             {
                 PlayingCards.Add(new PlayerCard(card));
-      
+
             }
+            //GroupBy does not change underlying collection
+
+            groupedByRank = PlayingCards.GroupBy(card => card.Rank)
+                            .OrderByDescending(group => group.Count());
+
             var json = JsonConvert.SerializeObject(PlayingCards);
         }
 
+
+        /*
+         *
+         *      PlayingCards = { card1, card2, card3, card4, card5,  } ;
+         *
+         *      GroupedBy = {   { card1, card2, }, {card4, card5}, { card3,}  }
+         *
+         */
+
         public string GetScore()
         {
-            //GroupedBy does not change underlying collection 
-            var groupedByRank = PlayingCards.GroupBy(x => x.Rank);
 
-            if (groupedByRank.Last().Count() == 2)
-             {
-                return $"Pair: {groupedByRank.Last().Last().Rank}s";
-             };
+            if (HaveWeGotOfAKind(4))
+            {
+                return $"Four of a kind: {GetBestRank()}s";
+            };
 
-            
+            if (HasTwoPairs())
+            {
+                return $"Two pair: {GetTwoPair().Item1}s and {GetTwoPair().Item2}s";
+            };
+
+
+            if (HaveWeGotOfAKind(3))
+            {
+                return $"Three of a kind: {GetBestRank()}s";
+            };
+
+            if (HaveWeGotOfAKind(2))
+            {
+                return $"Pair: {GetBestRank()}s";
+            };
+
             // Sort changes underlying collection
             PlayingCards.Sort(new CardRankComparer());
-            
+
             return $"high card: {PlayingCards.Last().Rank}";
+        }
+
+        private bool HasTwoPairs()
+        {
+           return groupedByRank.Where(group => group.Count() == 2)
+                .Count() == 2;
+        }
+
+        private (Rank Highest, Rank Lowest) GetTwoPair()
+        {
+            var pairs = groupedByRank.Where(group => group.Count() == 2);
+            var firstRank = pairs.First().First().Rank;
+            var secondRank = pairs.Last().First().Rank;
+
+            if (firstRank > secondRank)
+            {
+                return (firstRank, secondRank);
+            }
+
+            return (secondRank, firstRank);
+        }
+
+        private bool HaveWeGotOfAKind(int number)
+        {
+            return groupedByRank.First().Count() == number;
+        }
+
+        private Rank GetBestRank()
+        {
+            return groupedByRank.First().First().Rank;
         }
 
     }
--- PokerKata/UnitTests/PokerHandTests.cs
+++ PokerKata/UnitTests/PokerHandTests.cs
@@ -8,6 +8,12 @@ namespace PokerKata.UnitTests
         [InlineData("2C 3H 4S 8C AH", "high card: Ace")]
         [InlineData("2C 3H 4S 8C QH", "high card: Queen")]
         [InlineData("2C 3H 4S 8C 8H", "Pair: eights")]
+        [InlineData("2C 3H 3S 4C 8H", "Pair: threes")]
+        [InlineData("2C 3H 7S QH QC", "Pair: Queens")]
+        [InlineData("2C 3H QS QH QC", "Three of a kind: Queens")]
+        [InlineData("2C 7H 7S QH QC", "Two pair: Queens and sevens")]
+        [InlineData("2C QD QS QH QC", "Four of a kind: Queens")]
+
 
         public void AcceptPlayersHand_ReturnsHighestScore(string playersHand, string expectedScore)
         {
@@ -16,8 +22,9 @@ namespace PokerKata.UnitTests
             Assert.Equal(expectedScore, result);
         }
 
-    }
 
+
+    }
 }
 
 
